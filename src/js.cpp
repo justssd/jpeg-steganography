87f8e0a28488cfb1d9e36d8536c7744d32e391f4
Vas <163600875+justssd@users.noreply.github.com>
2024-09-05 19:03:40 -0700

    Parse command line arguments

--- src/js.cpp
+++ src/js.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
-#include <jpeglib.h>
+#include <fstream>
 
-int main()
+using namespace std::literals;
+
+void print_usage()
 {
-    std::cout << "Hello, World!\n";
-    return 0;
+    std::cerr << "Usage:\n"
+              << "      ./js embed cover.jpg payload.txt stego.jpg\n"
+              << "      ./js extract cover.jpg out.txt\n";
+}
+
+int main(int argc, char** argv)
+{
+    if (argc == 5 && argv[1] == "embed"sv )
+    {
+        std::ifstream cover(argv[2], std::ios::binary);
+        if (!cover)
+        {
+            std::cerr << "Could not open " << argv[2] << "\n";
+            return 1;
+        }
+
+        std::ifstream payload(argv[3], std::ios::binary);
+        if (!payload)
+        {
+            std::cerr << "Could not open " << argv[3] << "\n";
+            return 1;
+        }
+
+        std::ofstream stego(argv[4], std::ios::binary);
+        if (!stego)
+        {
+            std::cerr << "Could not open " << argv[4] << "\n";
+            return 1;
+        }
+    }
+    else if (argc == 4 && argv[1] == "extract"sv )
+    {
+        std::ifstream cover(argv[2], std::ios::binary);
+        if (!cover)
+        {
+            std::cerr << "Could not open " << argv[2] << "\n";
+            return 1;
+        }
+
+        std::ofstream out(argv[3], std::ios::binary);
+        if (!out)
+        {
+            std::cerr << "Could not open " << argv[3] << "\n";
+            return 1;
+        }
+    }
+    else
+    {
+        print_usage();
+        return 1;
+    }
 }
